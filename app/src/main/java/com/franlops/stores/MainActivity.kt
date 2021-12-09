0a0ca23cfbc8f58509dc8c304ecbb18e3a6f114b
Francisco Lopez <franlopbri@gmail.com>
2021-12-09 17:38:46 -0600

    Barra de Acciones

--- app/src/main/java/com/franlops/stores/MainActivity.kt
+++ app/src/main/java/com/franlops/stores/MainActivity.kt
@@ -39,6 +39,7 @@ class MainActivity : AppCompatActivity(), OnClickListener {
         val fragmentTransaction = fragmentManager.beginTransaction()
 
         fragmentTransaction.add(R.id.containerMain, fragment)
+        fragmentTransaction.addToBackStack(null)
         fragmentTransaction.commit()
 
         mBinding.fab.hide()
