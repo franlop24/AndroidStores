21d6fb52f881ced02c1777cce8ffd4f82d91bd82
Francisco Lopez <franlopbri@gmail.com>
2021-12-11 22:29:12 -0600

    Insertar tienda desde Frag,ent

--- app/src/main/java/com/franlops/stores/EditStoreFragment.kt
+++ app/src/main/java/com/franlops/stores/EditStoreFragment.kt
@@ -5,6 +5,8 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import com.franlops.stores.databinding.FragmentEditStoreBinding
 import com.google.android.material.snackbar.Snackbar
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
 
 
 class EditStoreFragment : Fragment() {
@@ -42,9 +44,17 @@ class EditStoreFragment : Fragment() {
                 true
             }
             R.id.action_save -> {
-                Snackbar.make(mBinding.root, getString(R.string.edit_store_message_save_success),
-                Snackbar.LENGTH_SHORT)
-                .show()
+                val store = StoreEntity(name = mBinding.etName.text.toString().trim(),
+                            phone = mBinding.etPhone.text.toString().trim(),
+                            website = mBinding.etWebsite.text.toString().trim())
+                doAsync {
+                    StoreApplication.database.storeDao().addStore(store)
+                    uiThread {
+                        Snackbar.make(mBinding.root, getString(R.string.edit_store_message_save_success),
+                            Snackbar.LENGTH_SHORT)
+                            .show()
+                    }
+                }
                 true
             }
             else -> super.onOptionsItemSelected(item)
